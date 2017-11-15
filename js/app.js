3708113c2495d9798c2450f12c444199f25dbf77
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2017-11-15 16:41:04 +0100

    create an array of cards

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
  * Create a list that holds all of your cards
  */
 
+var card = document.querySelectorAll('.card');
+console.log(card);
+
+var cards = [... card];
+console.log(cards[12]);
 
 /*
  * Display the cards on the page
