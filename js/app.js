808eb5b83bc2f9c95ffb951be023128f05c3d29f
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2017-11-16 17:36:36 +0100

    add type to each card

--- js/app.js
+++ js/app.js
@@ -5,15 +5,14 @@
 var cards = document.getElementsByClassName("card");
 console.log(cards);
 
-var cardFunction = function (){
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-};
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
 
-for (var i = 0; i < cards.length; i++){
-    cards[i].addEventListener("click", cardFunction);
-}
-
+// Shuffle function from http://stackoverflow.com/a/2450976
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -28,3 +27,17 @@ function shuffle(array) {
 
     return array;
 };
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+
