e7d5fc1021b85756049f8bb9076eb9bb7d0a5023
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2017-11-16 02:51:45 +0100

    remove comments

--- js/app.js
+++ js/app.js
@@ -9,17 +9,8 @@ cards.addEventListener("click", function () {
     cards.classList.toggle("open", "show");
 });
 
-var cards = [... card];
-console.log(cards[12]);
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -38,14 +29,4 @@ function shuffle(array) {
      this.classList.toggle('open');
      this.classList.toggle('show');
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
  }
