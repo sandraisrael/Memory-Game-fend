90061b34e1353174ae70754eab1e4e6174349255
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2017-11-28 20:08:00 +0100

    remove instruction comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
 
 // cards array holds all cards
 var card = document.getElementsByClassName("card");
@@ -24,14 +21,6 @@ var matchedCards = [...matchedCard];
  // stars list
  var starsList = document.querySelectorAll(".stars li");
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -75,19 +64,6 @@ function startGame(){
     timer.innerHTML = "0 mins 0 secs";
     clearInterval(interval);
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 
 // function to add and remove open and show class from cards so they display
 var displayCard = function (){
