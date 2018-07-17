795953f72aabfcdf64892ee4b1fb638369badb68
Pradeep Singh <viveksingh12126@gmail.com>
2018-07-18 02:04:17 +0530

    Removed unnecessary console.log statement.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 // cards array holds all cards
 let card = document.getElementsByClassName("card");
 let cards = [...card]
-console.log(cards);
 
 // deck of all cards in game
 const deck = document.getElementById("card-deck");
